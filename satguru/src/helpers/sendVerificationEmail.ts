ca34c1cc3434f09d37bc99fe7c8f40cd478a4c9a
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2025-06-13 22:07:58 +0530

    Email Verification Completed

--- satguru/src/helpers/sendVerificationEmail.ts
+++ satguru/src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,23 @@
-import { Resend } from "@lib/resend";
-import VerificationEmail from "@emails/verification";
+import { resend } from "../lib/resend";
+import { ApiResponse } from "../types/ApiResponse";
+import VerificationEmail from "../../email/VerificationEmail";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "Satguru || Verification Code",
+      react: VerificationEmail({username,otp: verifyCode}),);
+      
+    console.log("Email verification successful");
+    return { success: true, message: "verification email sent successfully" };
+  } catch (error) {
+    console.log("Error verification email", error);
+    return { success: false, message: "Error to verify email" };
+  }
+}
