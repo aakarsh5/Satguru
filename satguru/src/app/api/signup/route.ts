3f568a7770f79c54afcc1a6f824eee951439ce57
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2025-06-14 21:41:01 +0530

    Added signup

--- satguru/src/app/api/signup/route.ts
+++ satguru/src/app/api/signup/route.ts
@@ -1,2 +1,25 @@
 import dbConnect from "../../../lib/dbConnect";
 import User from "../../../model/User";
+import bcrypt from "bcryptjs";
+import {sendVerficationEmail } from '@/helpers/sendVerificationEmail';
+
+export async function POST(request: Request){
+    await dbConnect;
+
+    try{
+        const {username, email, passowrd} = await request.json()
+
+        const existingUserName = await User.findOne({
+            username,
+            verified: true,
+        })
+    }catch(error){
+        console.log("Error registering user", error);
+        return Response.json({
+            success: false,
+            message: "Error registering user"
+        },{
+            status:500, 
+        })
+    }
+}
