d65e4724d8638605f2b1063f99031c76cff8a5a7
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2026-09-25 18:17:04 +0545

    chore: remove previous nested Satguru app

--- satguru/src/schemas/messageSchema.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-
-export const messageSchema = z.object({
-  messageSchema: z.string().min(4, { message: "must contain atleast 4 words" }),
-});
--- satguru/src/types/ApiResponse.ts
+++
@@ -1,9 +0,0 @@
-//This file will define the interface of how the api should respond
-import { Message } from "../model/User";
-
-export interface ApiResponse {
-  success: boolean;
-  message: String;
-  isAcceptingMessages?: boolean;
-  messages?: Array<Message>;
-}
